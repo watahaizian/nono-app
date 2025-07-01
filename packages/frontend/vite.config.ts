8966c7ebf08e4fbc3195465019266cecacadefe7
watahaizian <watahaizian@gmail.com>
2025-07-01 16:20:40 +0900

    デプロイ準備

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,14 +1,10 @@
 import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-  define: {
-    'import.meta.env.VITE_BACKEND_URL': JSON.stringify(process.env.VITE_BACKEND_URL),
-    'import.meta.env.VITE_CLERK_PUBLISHABLE_KEY': JSON.stringify(process.env.VITE_CLERK_PUBLISHABLE_KEY),
-  },
 })
